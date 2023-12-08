6bc1820b714f6c08923d455798d45915d43ee336
Myke052 <maycol.galarza@correounivalle.edu.co>
2023-12-07 20:17:07 -0500

    intento

--- src/main/java/Controlador/ControladorUsers.java
+++ src/main/java/Controlador/ControladorUsers.java
@@ -17,7 +17,32 @@ import java.util.ArrayList;
  */
 public class ControladorUsers {
     Login lg;
-    
+    //Con este metodo verifica si existe el nombre y la contraseña en la base de datos
+    public void verificacion() throws SQLException
+    {
+        String sql = "SELECT * FROM producto";
+        Conexion c = new Conexion();
+        ResultSet rs = c.ejecutarConsulta(sql);
+        String usuarioIngresado = lg.getUsuario().getText();
+        String contraseñaIngresada = lg.getContraseña().getText();
+
+        while (rs.next()) 
+        {
+            String nombreUsuarioBD = rs.getString("Nombres");
+            String contraseñaBD = rs.getString("Contraseña");
+
+            if (usuarioIngresado.equals(nombreUsuarioBD) && contraseñaIngresada.equals(contraseñaBD)) 
+            {
+                // Usuario y contraseña coinciden con un registro en la base de datos
+                // Aquí puedes realizar las acciones correspondientes
+            } 
+            else 
+            {
+                // Usuario y/o contraseña no coinciden
+                // Puedes realizar acciones adicionales si es necesario
+            }
+        }
+    }
     
     //Con esto consulto todo lo que haya en la BD
  
