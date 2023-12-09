42514a94b9d22c10665185fee541eee5b30dfb74
PC <PC@DESKTOP-KOF9TOO.mshome.net>
2023-12-08 20:02:30 -0500

    Implementacion de la clase producto,mesa,pedido y sus atributos

--- src/main/java/Modelo/Pedido.java
+++ src/main/java/Modelo/Pedido.java
@@ -4,13 +4,73 @@
  */
 package Modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Myke
  */
 public class Pedido {
-    String imagen;
-    String nombre;
-    String contraseña;
+    Mesero MeseroEncargado; 
+    ArrayList<Producto> ProductosPedido = new ArrayList<>();
+    Mesa MesaPedido;
+    int TiempoEstimado;
+    String Estado;
+
+    public Pedido() {
+    }
+
+    public Pedido(Mesero MeseroEncargado, Mesa MesaPedido, int TiempoEstimado, String Estado) {
+        this.MeseroEncargado = MeseroEncargado;
+        this.MesaPedido = MesaPedido;
+        this.TiempoEstimado = TiempoEstimado;
+        this.Estado = Estado;
+    }
+
+    public Mesero getMeseroEncargado() {
+        return MeseroEncargado;
+    }
+
+    public void setMeseroEncargado(Mesero MeseroEncargado) {
+        this.MeseroEncargado = MeseroEncargado;
+    }
+
+    public ArrayList<Producto> getProductosPedido() {
+        return ProductosPedido;
+    }
+
+    public void setProductosPedido(ArrayList<Producto> ProductosPedido) {
+        this.ProductosPedido = ProductosPedido;
+    }
+
+    public Mesa getMesaPedido() {
+        return MesaPedido;
+    }
+
+    public void setMesaPedido(Mesa MesaPedido) {
+        this.MesaPedido = MesaPedido;
+    }
+
+    public int getTiempoEstimado() {
+        return TiempoEstimado;
+    }
+
+    public void setTiempoEstimado(int TiempoEstimado) {
+        this.TiempoEstimado = TiempoEstimado;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
     
+    
+    
+    
+    
+    
+ 
 }
---
+++ src/main/java/Modelo/Producto.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author PC
+ */
+public class Producto {
+    String nombre,descripcion,estado,imagen;
+    float precio; 
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, String descripcion, String estado, String imagen, float precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.imagen = imagen;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+}
